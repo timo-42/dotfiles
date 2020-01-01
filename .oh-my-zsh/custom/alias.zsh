6dae1c963e21be97359273597e226ab941f587a6
kenobi <kenobi@example.com>
2020-01-01 12:04:49 +0100

    move alias to sperarate file

---
+++ .oh-my-zsh/custom/alias.zsh
@@ -0,0 +1,6 @@
+# workspace git sync
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+# python
+alias vc="virtualenv --python=python3.7 venv"
+alias v="source venv/bin/activate"
