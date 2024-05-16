0c13637a3ed08c7cc77e0633fd718889ab44eb6b
Timo Haas <haastimo@gmx.de>
2024-05-16 22:12:45 +0200

    fixup! update

--- nvim/lua/kenobi/lazy.lua
+++ nvim/lua/kenobi/lazy.lua
@@ -12,38 +12,39 @@ if not vim.loop.fs_stat(lazypath) then
 end
 -- configure plugins
 vim.opt.rtp:prepend(lazypath)
-plugins ={
+plugins = {
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' }
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	{
 		-- provides ast
-		'nvim-treesitter/nvim-treesitter', build= ':TSUpdate'},
-	{"nvim-treesitter/playground"},
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{ "nvim-treesitter/playground" },
 	{ "rose-pine/neovim", name = "rose-pine" },
 
-{
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
-},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
--- LSP https://lsp-zero.netlify.app/v3.x/getting-started.html
---
-{'williamboman/mason.nvim'},
-{'williamboman/mason-lspconfig.nvim'},
-{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-{'neovim/nvim-lspconfig'},
-{'hrsh7th/cmp-nvim-lsp'},
-{'hrsh7th/nvim-cmp'},
-{'L3MON4D3/LuaSnip'},
+	-- LSP https://lsp-zero.netlify.app/v3.x/getting-started.html
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "L3MON4D3/LuaSnip" },
 
--- linter
-{'mfussenegger/nvim-lint'},
--- package manager for linter and formatter
-{'WhoIsSethDaniel/mason-tool-installer'},
+	-- linter
+	{ "mfussenegger/nvim-lint" },
+	-- package manager for linter and formatter
+	{ "WhoIsSethDaniel/mason-tool-installer" },
 }
-
 
 require("lazy").setup(plugins, opts)
